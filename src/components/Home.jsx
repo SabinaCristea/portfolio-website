36a06dcb283492dd49fdd1479bfdc2b7d1562e54
SabinaCristea <sabinacristea997@gmail.com>
2024-03-22 16:58:39 +0200

    Finished adding responsiveness on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,9 @@ import earthCoverImage from "../../src/assets/imgs/earth-cover.webp";
 function Home() {
   return (
     <div
-      className="flex flex-col items-center w-[100vw] h-[50vh] pt-[5rem] md:h-[90vh] md:pt-[14rem] md:mt-[-10rem]
+      className="flex flex-col items-center w-[100vw] h-[40vh] sm:h-[50vh] md:h-[70vh] lg:h-[90vh] pt-[15%] sm:pt-[10rem] md:pt-[25%] lg:pt-[10%] xl:pt-[12rem] 2xl:pt-[15rem] md:mt-[-10rem]
                  after:z-[-1] overlay-hidden
-                 after:h-[50vh] md:after:h-[90vh] after:w-[100%] after:absolute after:top-0 
+                 after:h-[40vh] sm:after:h-[50vh] md:after:h-[70vh] lg:after:h-[90vh] after:w-[100%] after:absolute after:top-0 
                  after:left-0 after:outline-dotted after:outline-[0.7rem] md:after:outline-[1rem] after:outline-[var(--color-green)]"
       style={{
         backgroundImage: `url(${earthCoverImage})`,
@@ -14,7 +14,7 @@ function Home() {
       }}
     >
       <p
-        className="text-[2rem] mb-[0] md:text-[2.4rem] uppercase tracking-[0.5rem] md:mb-[-4rem]"
+        className="text-[1.2rem] sm:text-[2rem] mb-[-2rem] sm:mb-[-3rem] md:text-[2.4rem] uppercase tracking-[0.3rem] md:tracking-[0.5rem] md:mb-[-4rem] text-center px-[5rem] "
         style={{ wordSpacing: "1rem" }}
       >
         Front-end portfolio of
@@ -22,7 +22,8 @@ function Home() {
       <img
         src="src/assets/svgs/handwritten-font.svg"
         alt="SVG Image"
-        className="w-[200%] md:w-[140%] lg:w-[90%] xl:w-[77%]"
+        className="w-[90%] sm:w-[80%] md:w-[100%] lg:w-[80%]"
+        // className=" w-[100%] md:w-[140%] lg:w-[90%] xl:w-[77%]"
       />
     </div>
   );
--- src/components/PresentationCard.jsx
+++ src/components/PresentationCard.jsx
@@ -3,33 +3,33 @@ import Button from "./Button";
 
 function PresentationCard() {
   return (
-    <div className="lg:mt-[5rem] lg:mb-[5rem] md:mt-[7rem] md:mb-[7rem] flex align-middle justify-center">
-      <div className="w-[80%] md:w-[90%] flex flex-col md:gap-[5rem] lg:gap-[4rem] lg:w-[90rem] lg:flex-row xl:justify-between md:px-[5rem] lg:px-[4rem] md:pt-0 lg:pt-[2rem] md:pb-0 lg:pb-[6rem] xl:px-[7rem] lg:bg-[var(--color-orange-transparent)] lg:rounded-[2rem] lg:outline-[1rem] lg:outline-dotted lg:outline-[var(--color-orange-transparent)] xl:w-[110rem] md:justify-center ">
-        <div className="flex flex-col lg:self-end md:gap-[5rem] lg:gap-[4rem] xl:gap-16">
-          <div className=" flex flex-col md:gap-[2rem] lg:gap-[4rem]">
-            <header className="text-[2rem] md:text-[5rem] lg:text-[4rem] xl:text-[5rem] drop-shadow-3xl md:text-center lg:text-left ">
+    <div className=" lg:mt-[10rem] lg:mb-[10rem] md:mt-[7rem] md:mb-[7rem] flex align-middle justify-center">
+      <div className="w-[80%] md:w-[90%] flex flex-col md:gap-[5rem] lg:gap-[4rem] lg:w-[90rem] lg:flex-row xl:justify-between px-[2rem] my-[6rem] md:px-[5rem] lg:px-[4rem] md:pt-0 lg:pt-[2rem] md:pb-0 lg:pb-[6rem] xl:px-[7rem] lg:bg-[var(--color-orange-transparent)] lg:rounded-[2rem] lg:outline-[1rem] lg:outline-dotted lg:outline-[var(--color-orange-transparent)] xl:w-[110rem] md:justify-center ">
+        <div className="flex flex-col lg:self-end gap-[6rem] md:gap-[5rem] lg:gap-[4rem] xl:gap-16">
+          <div className=" flex flex-col gap-[2rem] md:gap-[2rem] lg:gap-[4rem]">
+            <header className="text-[3rem] sm:text-[4rem] md:text-[5rem] lg:text-[4rem] xl:text-[5rem] drop-shadow-3xl text-center lg:text-left ">
               What it means to me
             </header>
-            <p className="drop-shadow-3xl md:max-w-[100%] md:text-[3.5rem] lg:text-[2.8rem] xl:text-[3.5rem] lg:max-w-[50rem] xl:max-w-[55rem] md:text-center lg:text-left">
+            <p className="drop-shadow-3xl md:max-w-[100%] text-[2rem] sm:text-[2.5rem] md:text-[3.5rem] lg:text-[2.8rem] xl:text-[3.5rem] lg:max-w-[50rem] xl:max-w-[55rem] text-center lg:text-left">
               Frontend development is the art of balance: blending aesthetics
               with functionality, elegance with efficiency.
             </p>
           </div>
-          <div className="flex items-center justify-center mt-[-5rem] lg:hidden">
+          <div className="flex items-center justify-center mt-[-5rem] md:mt-[-5rem]  lg:hidden">
             <img
               src="src/assets/imgs/me.webp"
               alt="Photo of me"
-              className="sm:rounded-b-2xl "
+              className="rounded-b-[0.7rem] sm:rounded-b-2xl "
             />
           </div>
-          <div className="flex flex-col md:gap-[1.5rem] lg:flex-row xl:justify-between md:w-[33.6rem] md:m-auto lg:w-[auto]">
+          <div className="flex flex-col gap-[1.5rem] lg:flex-row lg:justify-between w-[100%] sm:w-[33.6rem] m-auto lg:w-[auto]">
             <Button to={GITHUB_ACCOUNT}>
               <img
                 src="src/assets/svgs/icons8-github.svg"
                 alt="SVG Image"
                 className="max-w-[5rem]"
               />
-              <p className="md:text-[1.8rem] lg:text-[1.3rem] xl:text-[1.6rem]">
+              <p className="text-[1.4rem] md:text-[1.8rem] lg:text-[1.3rem] xl:text-[1.6rem] leading-[1.5rem]">
                 Find me on GitHub
               </p>
             </Button>
@@ -39,7 +39,7 @@ function PresentationCard() {
                 alt="SVG Image"
                 className="max-w-[5rem]"
               />
-              <p className="md:text-[1.8rem] lg:text-[1.3rem] xl:text-[1.6rem]">
+              <p className="text-[1.4rem] md:text-[1.8rem] lg:text-[1.3rem] xl:text-[1.6rem] leading-[1.5rem]">
                 Find me on LinkedIn
               </p>
             </Button>
