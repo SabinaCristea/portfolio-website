744f5515f469e625540bd9c0d8bb55d920dfc57b
SabinaCristea <sabinacristea997@gmail.com>
2024-03-28 16:40:06 +0200

    Modified getData function

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,5 +1,10 @@
-export async function getData() {
-  const res = await fetch("./skillsData.json");
-  const data = await res.json();
+// export async function getData() {
+//   const res = await fetch("./data.json");
+//   const data = await res.json();
+//   return data;
+// }
+import data from "../../data.json";
+
+export function getData() {
   return data;
 }
