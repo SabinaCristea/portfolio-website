a7a6d71c83a3d8f3ca5a31ee4bef2851a9da572e
SabinaCristea <sabinacristea997@gmail.com>
2024-08-03 19:02:07 +0300

    Updated social media accounts

--- src/utils/accounts.js
+++ src/utils/accounts.js
@@ -1,4 +1,5 @@
 export const LINKEDIN_ACCOUNT = "https://www.linkedin.com/in/sabina-cristea/";
 export const GITHUB_ACCOUNT = "https://github.com/SabinaCristea";
-export const INSTAGRAM_ACCOUNT = "https://www.instagram.com/__sabinacristea/";
-export const FACEBOOK_ACCOUNT = "https://facebook.com/sabina.cristea.92";
+export const INSTAGRAM_ACCOUNT = "https://www.instagram.com/sabinacodes/";
+export const FACEBOOK_ACCOUNT =
+  "https://www.facebook.com/profile.php?id=61563989422827";
